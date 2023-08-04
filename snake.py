a94bfa5eb3d67a40705175f197433505f96e7842
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2023-08-04 07:53:51 +0200

    pygame window and font

--- snake.py
+++ snake.py
@@ -10,3 +10,16 @@ FONT = pygame.font.Font("retro.ttf", BLOCK_SIZE * 2)
 screen = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("Snake!")
 clock = pygame.time.Clock()
+
+
+
+
+while True:
+    #event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(10)
