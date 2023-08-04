68c27d3a62b8eead65db85dc1201f791b77b0deb
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2023-08-04 07:57:23 +0200

    drawGrid()

--- snake.py
+++ snake.py
@@ -12,7 +12,13 @@ pygame.display.set_caption("Snake!")
 clock = pygame.time.Clock()
 
 
+def drawGrid():
+    for x in range(0, SW, BLOCK_SIZE):
+        for y in range(0, SH, BLOCK_SIZE):
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(screen, "#3c3c3b", rect, 1)
 
+drawGrid()
 
 while True:
     #event loop
